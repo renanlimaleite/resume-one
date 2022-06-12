f9705cc7a1eae03f22664ede80bc2e4cf6dd39e4
renanlimaleite <renan.lima.softwares@gmail.com>
2022-06-12 08:33:33 -0300

    add: .travisyml pre deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://renanlimaleite.github.io/resume-one/',
   plugins: [react(), tsConfigPaths()],
 });
