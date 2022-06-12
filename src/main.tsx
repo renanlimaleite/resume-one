c58f529d58eaf7ab480ece02aff71e27118f9ffc
renanlimaleite <renan.lima.softwares@gmail.com>
2022-06-12 03:30:56 -0300

    add: tailwind configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import './styles/index.css'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/index.css';
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById('root');
 
 if (!rootElement) {
-  throw new Error('Root element does not exist in your base HTML')
+  throw new Error('Root element does not exist in your base HTML');
 }
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
-import { Button } from '@/ui/Button'
+import { Button } from '@/ui/Button';
 
 export function Home() {
   return (
     <main>
       <Button />
-      <li>
+      <li className="text-3xl font-bold">
         Use a higher-level connected component. Storybook helps you compose such
         data from the args of child component stories
       </li>
     </main>
-  )
+  );
 }
